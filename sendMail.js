fc96e5e4126d835bd07e84614a819425fcc19b3b
yahya-cloud <59670962+yahya-cloud@users.noreply.github.com>
2021-03-13 23:17:08 +0530

    third commit

--- sendMail.js
+++ sendMail.js
@@ -1,27 +1,38 @@
+require('dotenv').config();
+
 const nodemailer = require('nodemailer');
 module.exports = function(app) {
 
     app.post('/send', async (req, res) => {
 
-        let testAccount = await nodemailer.createTestAccount();
+      const output = `
+      <h3>The person Info IS</h3>
+      <p>Name: ${req.body.name}</p>
+      <p>subject: ${req.body.subject}</p>
+      <p>email: ${req.body.email}</p>
+      
+      <h3>And the Message is</h3>
+      <p>${req.body.message}</p>
+      `
+       
         // create reusable transporter object using the default SMTP transport
         let transporter = nodemailer.createTransport({
-          host: "smtp.ethereal.email",
+          service: 'gmail',
           port: 587,
           secure: false, // true for 465, false for other ports
           auth: {
-            user: testAccount.user, // generated ethereal user
-            pass: testAccount.pass, // generated ethereal password
+            user: process.env.EMAIL, // generated ethereal user
+            pass: process.env.PASS, // generated ethereal password
           },
         });
     
     
          let mailOptions = {
             from: `${req.body.email}`, // sender address
-            to: "yahya08in@gmail.com", // list of receivers
+            to: "mollifywork@gmail.com", // list of receivers
             subject: `${req.body.subject}`, // Subject line
             text: `${req.body.message}`, // plain text body
-            html: "<h1>Email from nodemailer</h1>", // html body
+            html: output, // html body
           }
         
           // send mail with defined transport object
