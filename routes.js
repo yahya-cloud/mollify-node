0d90e8cd4cd3909c355162fead431cdca4b3ab60
yahya-cloud <59670962+yahya-cloud@users.noreply.github.com>
2021-03-15 14:40:48 +0530

    commiting refinement

--- routes.js
+++ routes.js
@@ -11,9 +11,6 @@ module.exports = function(app) {
     })
     .get('/career', function(req, res){
         res.render('pages/career', {title:"career", formAgain: false});
-    })
-    .get('/loaderio-dc8495f4e5d957629862cd56d6022112.html', function(req, res){
-        res.render('pages/loaderio-dc8495f4e5d957629862cd56d6022112')
-    })
+    }); 
     
 };
